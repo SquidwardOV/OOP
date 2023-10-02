57fee2afa6e36c7d14723c740044184774fab369
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2023-10-02 23:08:10 +0300

    Update Laba2.cpp

--- Laba2.cpp
+++ Laba2.cpp
@@ -4,12 +4,12 @@ Money.h
 
 class Money {
 private:
-    long rubles;
-    unsigned char kopecks;
+    long* rubles;
+    unsigned char* kopecks;
 
 public:
     Money();
-    Money(long rubles, unsigned char kopecks);
+    Money(long* rubles, unsigned char* kopecks);
     Money(const Money& other);
     ~Money();
 
@@ -30,46 +30,49 @@ public:
 
 Money.cpp
 
-  #include "money.h"
+#include "Money.h"
 
-Money::Money() : rubles(0), kopecks(0) {}
+Money::Money() : rubles(new long(0)), kopecks(new unsigned char(0)) {}
 
-Money::Money(long rubles, unsigned char kopecks) : rubles(rubles), kopecks(kopecks) {}
+Money::Money(long* rubles, unsigned char* kopecks) : rubles(rubles), kopecks(kopecks) {}
 
-Money::Money(const Money& other) : rubles(other.rubles), kopecks(other.kopecks) {}
+Money::Money(const Money& other) : rubles(new long(*other.rubles)), kopecks(new unsigned char(*other.kopecks)) {}
 
-Money::~Money() {}
+Money::~Money() {
+    delete rubles;
+    delete kopecks;
+}
 
 Money& Money::operator=(const Money& other) {
     if (this != &other) {
-        rubles = other.rubles;
-        kopecks = other.kopecks;
+        *rubles = *other.rubles;
+        *kopecks = *other.kopecks;
     }
     return *this;
 }
 
 Money Money::operator+(const Money& other) const {
-    long sum_rubles = rubles + other.rubles;
-    unsigned char sum_kopecks = kopecks + other.kopecks;
+    long sum_rubles = *rubles + *other.rubles;
+    unsigned char sum_kopecks = *kopecks + *other.kopecks;
     if (sum_kopecks >= 100) {
         sum_rubles += 1;
         sum_kopecks -= 100;
     }
-    return Money(sum_rubles, sum_kopecks);
+    return Money(new long(sum_rubles), new unsigned char(sum_kopecks));
 }
 
 Money Money::operator-(const Money& other) const {
-    long diff_rubles = rubles - other.rubles;
-    unsigned char diff_kopecks = kopecks - other.kopecks;
+    long diff_rubles = *rubles - *other.rubles;
+    unsigned char diff_kopecks = *kopecks - *other.kopecks;
     if (diff_kopecks < 0) {
         diff_rubles -= 1;
         diff_kopecks += 100;
     }
-    return Money(diff_rubles, diff_kopecks);
+    return Money(new long(diff_rubles), new unsigned char(diff_kopecks));
 }
 
 bool Money::operator==(const Money& other) const {
-    return (rubles == other.rubles) && (kopecks == other.kopecks);
+    return (*rubles == *other.rubles) && (*kopecks == *other.kopecks);
 }
 
 bool Money::operator!=(const Money& other) const {
@@ -77,11 +80,11 @@ bool Money::operator!=(const Money& other) const {
 }
 
 bool Money::operator<(const Money& other) const {
-    if (rubles < other.rubles) {
+    if (*rubles < *other.rubles) {
         return true;
     }
-    else if (rubles == other.rubles) {
-        return kopecks < other.kopecks;
+    else if (*rubles == *other.rubles) {
+        return *kopecks < *other.kopecks;
     }
     return false;
 }
@@ -91,12 +94,12 @@ bool Money::operator>(const Money& other) const {
 }
 
 std::ostream& operator<<(std::ostream& os, const Money& money) {
-    os << money.rubles << " рублей " << static_cast<int>(money.kopecks) << " копеек";
+    os << *money.rubles << " рублей " << static_cast<int>(*money.kopecks) << " копеек";
     return os;
 }
 
 std::istream& operator>>(std::istream& is, Money& money) {
-    is >> money.rubles >> money.kopecks;
+    is >> *money.rubles >> *money.kopecks;
     return is;
 }
 
@@ -104,15 +107,15 @@ std::istream& operator>>(std::istream& is, Money& money) {
 
 main.cpp
 
-  #include "Money.h"
+#include "Money.h"
 #include <iostream>
 #include <windows.h>
 
 int main() {
 
     SetConsoleOutputCP(1251);
-    Money money1(10, 50);
-    Money money2(5, 30);
+    Money money1(new long(10), new unsigned char(50));
+    Money money2(new long(5), new unsigned char(30));
 
     Money money3 = money1 + money2;
     Money money4 = money1 - money2;
